852d9e8f002067f1b98175c36118ddee0f2f02ad
KNA <knayad@gmail.com>
2024-05-06 20:16:25 -0400

    re-add .env info/ file to test server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
-const reviewRoutes = require("./backend/routes/reviews");
+const reviewRoutes = require("./routes/reviews");
 
 // express app
 const app = express();
@@ -21,13 +21,13 @@ app.use("/api/reviews", reviewRoutes);
 
 // connect to db
 mongoose
-  .connect(MONGO_URI)
-  // .then(() => {
-  //   // listen for request
-  //   app.listen(process.env.PORT, () => {
-  //     console.log("connected to the DB and listening on port 4000!");
-  //   });
-  // })
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(process.env.PORT, () => {
+      console.log("connected to the DB and listening on port 4000!");
+    });
+  })
   .catch((error) => {
     console.log(error);
   });
