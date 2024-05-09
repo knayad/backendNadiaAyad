58438630dd110302438e2c9662fa60af967073b7
KNA <knayad@gmail.com>
2024-05-09 07:39:16 -0400

    temp remove middleware

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // routes (technically middleware); no last slash
-app.use("/api/reviews", reviewRoutes);
+// app.use("/api/reviews", reviewRoutes);
 
 // connect to db
 mongoose
